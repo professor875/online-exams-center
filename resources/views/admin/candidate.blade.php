8cae0e3cb20a2d1092457f32cdad59e204955e80
Abdul Haseeb Khan <mubshirk875@gmail.com>
2023-12-25 22:52:14 +0500

    candidate

--- resources/views/admin/candidate.blade.php
+++ resources/views/admin/candidate.blade.php
@@ -5,7 +5,7 @@
         <header class=" dark:bg-gray-800 shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h2 class=" font-bold text-xl text-red-500 leading-tight">
-                    Admin Dashboard
+                    Candidates
                 </h2>
             </div>
         </header>
