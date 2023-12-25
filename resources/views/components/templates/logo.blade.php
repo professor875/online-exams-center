502885614fcc9b04ec27b71c0e58325e01520679
Abdul Haseeb Khan <mubshirk875@gmail.com>
2023-12-25 17:17:03 +0500

    updated logo

--- resources/views/components/templates/logo.blade.php
+++ resources/views/components/templates/logo.blade.php
@@ -1,4 +1,14 @@
-<div class="flex justify-center space-x-2 items-center hover:brightness-75 transition-all cursor-pointer">
-    <img class="" src="{{ asset('images/logo.png') }}">
-    <p class=" hidden sm:flex text-xl font-bold text-red-500">Examination Hall !</p>
+<div x-data="{ open: true }" @click.outside="open = true" @close.stop="open = false" class="flex justify-center space-x-2 items-center hover:brightness-150 transition-all cursor-pointer">
+    <img @click="open = !open" src="{{ asset('images/logo.png') }}">
+    <p x-show="open"
+       x-transition:enter="transition ease-out duration-600"
+       x-transition:enter-start="opacity-0 scale-95"
+       x-transition:enter-end="opacity-100 scale-100"
+       x-transition:leave="transition ease-in duration-6005"
+       x-transition:leave-start="opacity-100 scale-100"
+       x-transition:leave-end="opacity-0 scale-95"
+       class="hidden sm:flex transition-all text-xl font-bold text-red-500"
+       style="display: none;"
+       @click="open = false">
+        Examination Hall !</p>
 </div>
