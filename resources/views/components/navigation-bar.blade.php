bdae16a917a6ad726460128df1650b4916ecb0d2
Abdul Haseeb Khan <mubshirk875@gmail.com>
2023-12-25 19:31:56 +0500

    hover

--- resources/views/components/navigation-bar.blade.php
+++ resources/views/components/navigation-bar.blade.php
@@ -62,10 +62,11 @@
                 <x-templates.dropedown-link name="Profile"/>
                 @if( auth()->user()->roll === 'admin')
                     <x-templates.dropedown-link name="Dashboard"/>
+                    <x-templates.dropedown-link name="Create Admin"/>
                 @endif
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <button class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
+                    <button class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out hover:text-red-500">
                         Logout
                     </button>
                 </form>
