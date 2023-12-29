fbac9bac97166b5b7687b279f108e3ef23f3d70b
Abdul Haseeb Khan <mubshirk875@gmail.com>
2023-12-29 20:39:45 +0500

    exams

--- resources/views/exam/exams.blade.php
+++ resources/views/exam/exams.blade.php
@@ -1,33 +1,21 @@
 <x-layout>
-    @foreach( $exams as $exam )
 
-    <div class=" p-6 text-red-500 rounded shadow-md">
-            <div class=" lg:flex justify-between items-center lg:space-x-8">
-                <h1 class=" my-5 text-xl p-2 w-full "> Examiner : {{ $exam->examiner }}</h1>
-
-                <h1 class=" my-5 text-xl p-2 w-full "> Title : {{ $exam->title }}</h1>
-
-                <h1 class=" my-5 text-xl p-2 w-full "> Duration : {{ $exam->duration }} Minutes</h1>
-            </div>
-
-        <h2 class="text-lg font-semibold mb-2">MCQ's</h2>
-
-            @foreach( $exam->questions as $question )
-                <div class="mb-4">
-                    <h1 class=" my-5 text-lg p-2 w-full "> Question : {{ $question->question }}</h1>
-
-                    <div class=" xl:flex xl:space-x-14 justify-around">
-                        @foreach( $question->answers as $answer )
-                            <div class=" underline underline-offset-8 cursor-pointer min-w-64 max-w-72 flex items-center mb-2">
-                                <h1 class=" my-5 text-lg p-2 w-full ">{{ $answer->answer }}</h1>
-                                    <input type="radio" name="questions[correct_answer]" required class="mr-1">
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+        <div class="flex py-16 mx-44 min-h-screen justify-around text-red-500 overflow-auto space-x-8">
+            @foreach( $exams as $exam )
+                    <a href="/view/{{ $exam->id }}" class=" p-8 min-w-72 max-h-96 cursor-pointer border hover:outline hover:outline-red-500 border-red-500 rounded-xl ">
+                        <h1 class="text-lg font-bolded">
+                            Number: <br> {{ $exam->id }}
+                        </h1>
+                        <h1 class="text-lg font-bolded">
+                            Title: <br> {{ $exam->title }}
+                        </h1>
+                        <h1 class="text-lg mt-8 font-bolded">
+                            Examiner: <br> {{ $exam->examiner }}
+                        </h1>
+                        <h1 class="text-lg mt-8 font-bolded">
+                            Duration: <br> {{ $exam->duration }} Minutes
+                        </h1>
+                    </a>
             @endforeach
-
-            <x-form.primary-btn name="Submit"/>
-    </div>
-    @endforeach
+        </div>
 </x-layout>
