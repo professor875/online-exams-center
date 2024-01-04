45cc1098d5a2880a764cd68f9c376e3816448c77
Abdul Haseeb Khan <mubshirk875@gmail.com>
2024-01-04 15:17:13 +0500

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="footer w-full bg-gray-800 mt-40 p-4">
+<div class="footer w-full bg-gray-800 mt-16 p-4">
     <div class="text-center">
         <h1 class="text-2xl text-white font-semibold">🌐 QuickQuizCenter - Connect. Learn. Excel.</h1>
         <p class="text-sm text-gray-300 my-4">QuickQuizCenter is your all-in-one platform for seamless online assessments. Whether you're a student eager to test your knowledge <br> or a teacher looking to create engaging exams, we've got you covered.</p>
