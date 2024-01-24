648d0d072e4a5066782e6447d66b0acc183b7c0b
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2024-01-24 23:02:45 +0500

    creating svg laravel

--- resources/views/auth/quick-quiz.blade.php
+++ resources/views/auth/quick-quiz.blade.php
@@ -11,10 +11,8 @@
     <div class="grid grid-cols-4 gap-8 py-10 mx-44 min-h-screen text-red-500">
             <a href="#" target="_blank" class=" p-8 max-h-[200px] transition-all cursor-pointer border hover:outline hover:outline-red-500 border-red-500 rounded-xl ">
                 <div class="logo">
+
                 </div>
-                <h1 class="text-lg mt-8 font-bolded">
-                    Duration: <br> 3 Minutes
-                </h1>
             </a>
     </div>
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Quick Quiz's Center</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png" >
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
